227700f5c98f498cf9eac154f844843d09b1d68a
hcharon <hcharon@softia.fr>
2025-05-09 11:30:21 +0200

    feat actif themes only in /me

--- front/src/app/pages/theme-list/theme-list.component.ts
+++ front/src/app/pages/theme-list/theme-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Theme} from "../../interfaces";
 import {Router} from "@angular/router";
 import {ThemeListService} from "../../service/ThemeListService";
@@ -9,13 +9,14 @@ import {ThemeListService} from "../../service/ThemeListService";
   styleUrls: ['./theme-list.component.scss']
 })
 export class ThemeListComponent implements OnInit {
+  @Input() onlySubscribed: boolean = false;
+
   themes: Theme[] = [];
 
   constructor(
     private themeListService: ThemeListService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.loadThemes();
@@ -23,23 +24,32 @@ export class ThemeListComponent implements OnInit {
 
   loadThemes() {
     this.themeListService.getListThemes().subscribe({
-      next: (data: Theme[]) => this.themes = data,
-      error: (err: String) => console.error('Erreur lors de l\'affichage des thèmes', err)
-    })
+      next: (data: Theme[]) => {
+        this.themes = this.onlySubscribed
+          ? data.filter(theme => theme.subscribed)
+          : data;
+      },
+      error: (err: string) => console.error('Erreur lors de l\'affichage des thèmes', err)
+    });
   }
+
   goToDetail(id: number): void {
     this.router.navigate(['/theme/detail/', id]);
   }
+
   subscribeToTheme(theme: Theme): void {
     this.themeListService.manageSubscribe(theme.id).subscribe({
       next: () => {
         theme.subscribed = !theme.subscribed;
-        console.log(` ${theme.subscribed ? 'Abonné' : 'Désabonné'} au thème`, theme.id);
+        console.log(`${theme.subscribed ? 'Abonné' : 'Désabonné'} au thème`, theme.id);
+
+        if (this.onlySubscribed && !theme.subscribed) {
+          this.themes = this.themes.filter(t => t.id !== theme.id);
+        }
       },
       error: (err) => {
-        console.error(' Erreur abonnement :', err);
+        console.error('Erreur abonnement :', err);
       }
     });
   }
-
 }
